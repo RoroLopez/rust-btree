089a2319aceab649832fb9b99afe376ce666212e
RoroLopez <rorolopezag@gmail.com>
2024-12-22 20:05:29 -0600

    add split root operation

--- src/main.rs
+++ src/main.rs
@@ -37,37 +37,53 @@ impl BTree {
         todo!()
     }
 
-    // fn split_child(&mut self, parent: &mut BTreeNode, index: usize) --> original signature
-    fn split_child(&mut self, index: usize) {
-        let t = self.degree;
-        let mut parent = &mut self.root;
+    fn merge_node() {
+        todo!()
+    }
+
+    fn insert_not_full() {
+        todo!()
+    }
+
+    fn new_node(degree: usize, leaf: bool) -> BTreeNode {
+        BTreeNode {
+            keys: vec![None; 2*degree - 1],
+            children: vec![None; 2*degree],
+            is_leaf: leaf
+        }
+    }
+
+    fn split_root(&mut self) {
+        let mut new_root = Self::new_node(self.degree, false);
+        let old_root = std::mem::replace(&mut self.root, Self::new_node(self.degree, false));
+        new_root.children[0] = Some(old_root);
+        self.root = new_root;
+        Self::split_child(&mut self.root, 0, self.degree);
+    }
+
+    fn split_child(parent: &mut BTreeNode, index: usize, degree: usize) {
         let mut full_node = parent.children[index].take().unwrap();
-        let mut new_node: BTreeNode = BTreeNode {
-            keys: vec![None; self.key_size],
-            children: vec![None; self.children_size],
-            is_leaf: full_node.is_leaf,
-        };
-        let new_node_keys: Vec<Option<u32>> = full_node.keys.iter_mut().skip(t).map(Option::take).collect();
+        let mut new_node = Self::new_node(degree, full_node.is_leaf);
+        let new_node_keys: Vec<Option<u32>> = full_node.keys.iter_mut().skip(degree).map(Option::take).collect();
         for (i, key) in new_node_keys.into_iter().enumerate() {
             new_node.keys[i] = key;
         }
         if !full_node.is_leaf {
-            let new_node_children: Vec<Option<BTreeNode>> = full_node.children.iter_mut().skip(t).map(Option::take).collect();
+            let new_node_children: Vec<Option<BTreeNode>> = full_node.children.iter_mut().skip(degree).map(Option::take).collect();
             for (i, child) in new_node_children.into_iter().enumerate() {
                 new_node.children[i] = child;
             }
         }
-        // shift children to make space for new child
-        for i in (index+2..self.children_size).rev() {
+
+        for i in (index+2..2*degree).rev() {
             parent.children[i] = parent.children[i-1].take();
         }
         parent.children[index+1] = Some(new_node);
 
-        // shift keys one element to make space for new key
-        for i in (index+1..self.key_size).rev() {
+        for i in (index+1..2*degree-1).rev() {
             parent.keys[i] = parent.keys[i-1].take();
         }
-        parent.keys[index] = full_node.keys[t-1].take(); // median value of node
+        parent.keys[index] = full_node.keys[degree-1].take();
         parent.children[index] = Some(full_node);
     }
 }
@@ -77,31 +93,19 @@ fn main() {
     let mut root_node = BTreeNode {
         keys: vec![None; 2*t - 1],
         children: vec![None; 2*t],
-        is_leaf: false,
-    };
-    root_node.keys[0] = Some(4);
-    root_node.keys[1] = Some(10);
-
-    let mut split_node = BTreeNode {
-        keys: vec![None; 2*t - 1],
-        children: vec![None; 2*t],
         is_leaf: true,
     };
-    split_node.keys[0] = Some(5);
-    split_node.keys[1] = Some(6);
-    split_node.keys[2] = Some(7);
-    split_node.keys[3] = Some(8);
-    split_node.keys[4] = Some(9);
-
-    root_node.children[1] = Some(split_node);
-
+    root_node.keys[0] = Some(5);
+    root_node.keys[1] = Some(6);
+    root_node.keys[2] = Some(7);
+    root_node.keys[3] = Some(8);
+    root_node.keys[4] = Some(9);
     let mut tree = BTree {
+        root: root_node,
         degree: t,
         key_size: 2*t-1,
         children_size: 2*t,
-        root: root_node,
     };
-
-    tree.split_child( 1);
+    tree.split_root();
     println!("{:?}", tree);
 }
