1409b126371c10f111accb17f5c4e097ff7dbcfc
RoroLopez <rorolopezag@gmail.com>
2024-12-24 20:37:55 -0600

    implement insert_not_full operation and tested it

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct BTree {
 }
 
 impl BTree {
-    pub fn create_tree(degree: usize) -> Self {
+    pub fn create_tree(&self, degree: usize) -> Self {
         let root = BTreeNode {
             keys: vec![None; 2*degree - 1],
             children: vec![None; 2*degree],
@@ -29,8 +29,12 @@ impl BTree {
         todo!()
     }
 
-    pub fn insert(key: u32) {
-        todo!()
+    pub fn insert(&mut self, key: u32) {
+        let root_vector_size = self.root
+            .keys
+            .iter()
+            .filter(|el| el.is_some())
+            .count();
     }
 
     pub fn remove(key: u32) {
@@ -41,8 +45,34 @@ impl BTree {
         todo!()
     }
 
-    fn insert_not_full() {
-        todo!()
+    fn insert_not_full(&self, node: &mut BTreeNode, key: u32, degree: usize) {
+        let mut i = 0;
+        while i < node.keys.len() && node.keys[i].is_some() && key > node.keys[i].unwrap() {
+            i += 1;
+        }
+        if node.is_leaf {
+            // TODO refactor shift of elements
+            for index in (i+1..node.keys.len()).rev() {
+                node.keys[index] = node.keys[index-1].take();
+            }
+            node.keys[i] = Some(key);
+        } else {
+            // TODO refactor count of keys
+            let child_vector_size = node.children[i]
+                .as_ref()
+                .unwrap()
+                .keys
+                .iter()
+                .filter(|el| el.is_some())
+                .count();
+            if child_vector_size == node.children[i].as_ref().unwrap().keys.capacity() {
+                Self::split_child(node, i, degree);
+                if key > node.keys[i].unwrap() {
+                    i += 1;
+                }
+            }
+            Self::insert_not_full(self, node.children[i].as_mut().unwrap(), key, degree);
+        }
     }
 
     fn new_node(degree: usize, leaf: bool) -> BTreeNode {
@@ -75,11 +105,13 @@ impl BTree {
             }
         }
 
+        // TODO refactor shift of keys
         for i in (index+2..2*degree).rev() {
             parent.children[i] = parent.children[i-1].take();
         }
         parent.children[index+1] = Some(new_node);
 
+        // TODO refactor shift of children
         for i in (index+1..2*degree-1).rev() {
             parent.keys[i] = parent.keys[i-1].take();
         }
@@ -89,23 +121,59 @@ impl BTree {
 }
 
 fn main() {
-    let t = 3;
-    let mut root_node = BTreeNode {
+    let t = 2;
+    let mut root = BTreeNode {
+        keys: vec![None; 2*t - 1],
+        children: vec![None; 2*t],
+        is_leaf: false,
+    };
+    root.keys[0] = Some(4);
+
+    let mut child1 = BTreeNode {
+        keys: vec![None; 2*t - 1],
+        children: vec![None; 2*t],
+        is_leaf: false,
+    };
+
+    child1.keys[0] = Some(6);
+
+    let mut child2 = BTreeNode {
         keys: vec![None; 2*t - 1],
         children: vec![None; 2*t],
         is_leaf: true,
     };
-    root_node.keys[0] = Some(5);
-    root_node.keys[1] = Some(6);
-    root_node.keys[2] = Some(7);
-    root_node.keys[3] = Some(8);
-    root_node.keys[4] = Some(9);
-    let mut tree = BTree {
-        root: root_node,
-        degree: t,
-        key_size: 2*t-1,
-        children_size: 2*t,
+
+    child2.keys[0] = Some(5);
+
+    let mut child3 = BTreeNode {
+        keys: vec![None; 2*t - 1],
+        children: vec![None; 2*t],
+        is_leaf: true,
     };
-    tree.split_root();
-    println!("{:?}", tree);
+
+    child3.keys[0] = Some(7);
+    child3.keys[1] = Some(8);
+    child3.keys[2] = Some(9);
+
+    child1.children[0] = Some(child2);
+    child1.children[1] = Some(child3);
+    root.children[1] = Some(child1);
+
+    let mut tree = BTree {
+        degree: 2,
+        key_size: 2*2 - 1,
+        children_size: 2*2,
+        root,
+    };
+
+    let empty_node = BTreeNode {
+        keys: vec![None; 2*2-1],
+        children: vec![None; 2*2],
+        is_leaf: true,
+    };
+
+    let mut old_root = std::mem::replace(&mut tree.root, empty_node);
+    
+    tree.insert_not_full(&mut old_root, 10, 2);
+    println!("{:?}", old_root);
 }
